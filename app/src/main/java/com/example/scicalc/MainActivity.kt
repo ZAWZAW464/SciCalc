bcb76d9cf9f41f164602932adec5f6e0439e11da
ZAWZAW464 <nyin74472@gmail.com>
2026-07-11 22:21:57 +0630

    feat: add MainActivity.kt

---
+++ app/src/main/java/com/example/scicalc/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.scicalc
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.scicalc.domain.CalculatorViewModel
+import com.example.scicalc.ui.screens.CalculatorScreen
+import com.example.scicalc.ui.theme.SciCalcTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SciCalcTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    val viewModel: CalculatorViewModel = viewModel()
+                    CalculatorScreen(viewModel = viewModel)
+                }
+            }
+        }
+    }
+}
