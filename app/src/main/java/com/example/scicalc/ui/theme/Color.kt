2815864ffd162d535198b9dd436efd37d6ea2ca6
ZAWZAW464 <nyin74472@gmail.com>
2026-07-11 22:25:01 +0630

    feat: add Color.kt (theme definitions)

---
+++ app/src/main/java/com/example/scicalc/ui/theme/Color.kt
@@ -0,0 +1,77 @@
+package com.example.scicalc.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Light Theme Colors
+
+val LightPrimary = Color(0xFF1A6B4F)
+val LightOnPrimary = Color(0xFFFFFFFF)
+val LightPrimaryContainer = Color(0xFFA7F5CD)
+val LightOnPrimaryContainer = Color(0xFF002114)
+
+val LightSecondary = Color(0xFF4C6354)
+val LightOnSecondary = Color(0xFFFFFFFF)
+val LightSecondaryContainer = Color(0xFFCEE9D5)
+val LightOnSecondaryContainer = Color(0xFF092014)
+
+val LightTertiary = Color(0xFF3D6471)
+val LightOnTertiary = Color(0xFFFFFFFF)
+val LightTertiaryContainer = Color(0xFFC1E8F9)
+val LightOnTertiaryContainer = Color(0xFF001F27)
+
+val LightSurface = Color(0xFFF8FBF5)
+val LightOnSurface = Color(0xFF191D1A)
+val LightSurfaceVariant = Color(0xFFDBE5DB)
+val LightOnSurfaceVariant = Color(0xFF404942)
+val LightSurfaceContainerLow = Color(0xFFF2F6EF)
+
+val LightError = Color(0xFFBA1A1A)
+val LightOnError = Color(0xFFFFFFFF)
+
+val LightOutline = Color(0xFF707972)
+
+// Dark Theme Colors
+
+val DarkPrimary = Color(0xFF8CD9B2)
+val DarkOnPrimary = Color(0xFF003823)
+val DarkPrimaryContainer = Color(0xFF005237)
+val DarkOnPrimaryContainer = Color(0xFFA7F5CD)
+
+val DarkSecondary = Color(0xFFB3CCBA)
+val DarkOnSecondary = Color(0xFF1E352A)
+val DarkSecondaryContainer = Color(0xFF354B3E)
+val DarkOnSecondaryContainer = Color(0xFFCEE9D5)
+
+val DarkTertiary = Color(0xFFA6CCDC)
+val DarkOnTertiary = Color(0xFF063543)
+val DarkTertiaryContainer = Color(0xFF244B58)
+val DarkOnTertiaryContainer = Color(0xFFC1E8F9)
+
+val DarkSurface = Color(0xFF191D1A)
+val DarkOnSurface = Color(0xFFE1E3DF)
+val DarkSurfaceVariant = Color(0xFF404942)
+val DarkOnSurfaceVariant = Color(0xFFC0C9C0)
+val DarkSurfaceContainerLow = Color(0xFF151916)
+
+val DarkError = Color(0xFFFFB4AB)
+val DarkOnError = Color(0xFF690005)
+
+val DarkOutline = Color(0xFF8A938B)
+
+// Custom Calculator Colors
+
+val NumberButtonLight = Color(0xFFF0F4EE)
+val NumberButtonDark = Color(0xFF2D332E)
+val OperatorButtonLight = Color(0xFFE0F2E7)
+val OperatorButtonDark = Color(0xFF1A4B38)
+val FunctionButtonLight = Color(0xFFDCE8E0)
+val FunctionButtonDark = Color(0xFF2A3D32)
+val EqualsButtonLight = Color(0xFF1A6B4F)
+val EqualsButtonDark = Color(0xFF8CD9B2)
+
+val DisplayBgLight = Color(0xFFF0F7F1)
+val DisplayBgDark = Color(0xFF121714)
+val DisplayTextLight = Color(0xFF191D1A)
+val DisplayTextDark = Color(0xFFE1E3DF)
+val ExpressionTextLight = Color(0xFF5A655D)
+val ExpressionTextDark = Color(0xFF9AA89B)
