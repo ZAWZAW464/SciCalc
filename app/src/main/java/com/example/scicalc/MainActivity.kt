85627e96442886ea8dfd23d56395668f95cc0cea
ZAWZAW464 <nyin74472@gmail.com>
2026-07-18 23:50:17 +0630

    Optimize MainActivity for fast launch

--- app/src/main/java/com/example/scicalc/MainActivity.kt
+++ app/src/main/java/com/example/scicalc/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.scicalc
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,6 +15,7 @@ import com.example.scicalc.ui.theme.SciCalcTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
         setContent {
             SciCalcTheme {
                 Surface(modifier = Modifier.fillMaxSize()) {
