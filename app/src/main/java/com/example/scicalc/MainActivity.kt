eaeed77c53aa9fddc3daa3534f4e150356dba7a0
ZAWZAW464 <nyin74472@gmail.com>
2026-07-18 23:21:45 +0630

    Remove enableEdgeToEdge to fix splash screen white screen delay

--- app/src/main/java/com/example/scicalc/MainActivity.kt
+++ app/src/main/java/com/example/scicalc/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.scicalc
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
@@ -15,7 +14,6 @@ import com.example.scicalc.ui.theme.SciCalcTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             SciCalcTheme {
                 Surface(modifier = Modifier.fillMaxSize()) {
